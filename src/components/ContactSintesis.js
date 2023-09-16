cf5905f898fae233f5d5e214228393a52a7f85df
steve-ux <exe.universal@gmail.com>
2023-09-16 00:45:58 -0300

    no funcionó la page de success, se prueba nuevo código

--- src/components/ContactSintesis.js
+++ src/components/ContactSintesis.js
@@ -1,7 +1,22 @@
 import "../styles/formHome.scss";
 import DataCenter from "../assets/datacenter-creativo.jpeg";
+import { useNavigate } from "react-router-dom";
 
 function ContactSintesis() {
+
+  let navigate = useNavigate()
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    let myForm = document.getElementById("contact-form");
+    let formData = new FormData(myForm);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => navigate('/follow-up'))
+      .catch((error) => alert(error));
+  }
     return (
     <div className="contactSintesis">
         <div class="container">
@@ -11,7 +26,7 @@ function ContactSintesis() {
     <div class="container-text">
       <h2>[ Permanezcamos conectados ]</h2>
       <p>Dejanos tu correo electrónico y te contactamos a la brevedad. <br/>Además te suscribirás al newsletter de PixelPeak para enterarte siempre de las últimas novedades que tenemos para vos.<br/> Prometemos no mandarte spam.</p>
-      <form action="/success" name="contact" method="POST" data-netlify="true" onSubmit="submit">
+      <form onSubmit={submitHandler} id="contact-form" action="/success" name="contact" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact"/>
       <input type="email" id="email" name="Correo" placeholder="Email" required/>
       <button type="submit">Suscribirse</button>
