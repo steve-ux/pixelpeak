e4e1858e49fbce302e76c9e61b4d1f977d7004c7
steve-ux <exe.universal@gmail.com>
2023-09-15 22:33:48 -0300

    se está probando nuevo backend para los formularios

--- src/components/ContactSintesis.js
+++ src/components/ContactSintesis.js
@@ -11,12 +11,13 @@ function ContactSintesis() {
     <div class="container-text">
       <h2>[ Permanezcamos conectados ]</h2>
       <p>Dejanos tu correo electrónico y te contactamos a la brevedad. <br/>Además te suscribirás al newsletter de PixelPeak para enterarte siempre de las últimas novedades que tenemos para vos.<br/> Prometemos no mandarte spam.</p>
-      <form action="https://formsubmit.co/exe.universal@gmail.com" method="post">
+      <form name="correo" method="POST" netlify>
       <input type="email" id="email" name="Correo" placeholder="Email" required/>
       <button type="submit">Suscribirse</button>
       <input type="hidden" name="_next" value="#"/>
     <input type="hidden" name="_subject" value="Nuevo correo para el newsletter. Mandar info."/>
     <input type="hidden" name="_captcha" value="false" />
+    <input type="hidden" name="_template" value="box"/>
       </form>
     </div>
   </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,55 @@
+import React, { useState } from "react";
 import "../styles/form.scss";
 
 function Formulario() {
+
+  const [enviado, setEnviado] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes hacer cualquier lógica de envío que necesites.
+    // Estoy simulando un envío exitoso después de 2 segundos.
+    setTimeout(() => {
+      setEnviado(true);
+      // Después de 2 segundos, oculta el formulario
+      document.querySelector(".formContact").classList.add("sent");
+    }, 2000);
+  };
+
     return (
-    <div className="formContact">
+      <>
+    <div className={`formContact ${enviado ? "sent" : ""}`}>
   <div class="containerForm">
   <div class="left">
     <div class="headerForm">
       <h2 class="animation a1">[ Comencemos ]</h2>
       <h4 class="animation a2">Completalo y nos comunicamos a la brevedad. Los campos con * son obligatorios.</h4>
     </div>
+    <form onSubmit={handleSubmit} name="data" method="POST" netlify>
     <div class="form">
-    <input type="text" class="form-field animation a3" placeholder="*Nombre" required/>
-      <input type="email" class="form-field animation a3" placeholder="*Email" required/>
-      <input type="number" class="form-field animation a3" placeholder="*Teléfono. Ej: +54 9 261 212 1844" required/>
-      <input type="text" class="form-field animation a4" placeholder="Tu marca o empresa"/>
-      <textarea type="text" class="form-field animation a3" placeholder="Idea detallada"/>
+    <input type="text" id="text" name="Nombre" class="form-field animation a3" placeholder="*Nombre" required/>
+      <input type="email" id="email" name="Correo" class="form-field animation a3" placeholder="*Email" required/>
+      <input type="number" id="number" name="Teléfono" class="form-field animation a3" placeholder="*Teléfono. Ej: +54 9 261 212 1844" required/>
+      <input type="text" id="texto" name="Marca o proyecto" class="form-field animation a4" placeholder="Tu marca o empresa"/>
+      <textarea type="text" id="message" name="Mensaje" class="form-field animation a3" placeholder="Idea detallada"/>
       {/* <p class="animation a5"><a href="#">Al enviar el formulario aceptas los términos y condiciones.</a></p> */}
-      <button class="animation a6">ENVIAR</button>
+      <button type="submit" class="animation a6">ENVIAR</button>
+      <input type="hidden" name="_next" value="#"/>
+    <input type="hidden" name="_subject" value="Nuevo correo para el newsletter. Mandar info."/>
+    <input type="hidden" name="_captcha" value="false" />
+    <input type="hidden" name="_template" value="table"/>
     </div>
+    </form>
   </div>
   <div class="right"></div>
 </div>
     </div>
+    {enviado && (
+        <div className="mensajeExito">
+          Gracias por enviar su mensaje. Nos pondremos en contacto pronto.
+        </div>
+      )}
+      </>
     );
   }
   
